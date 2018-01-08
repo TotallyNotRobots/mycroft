68aa845583b7ac6e54e91c66a754d72d2f39335a
linuxdaemon <linuxdaemon@snoonet.org>
2018-01-08 09:21:10 -0600

    Remove unneeded plugins, config options, and data files

--- plugins/dig.py
+++
@@ -1,7 +0,0 @@
-from cloudbot import hook
-
-
-@hook.command()
-def dig():
-    """- This command is retired"""
-    return "The jsondns API no longer exists. This command is retired."
--- tests/plugin_tests/test_lmgtfy.py
+++
@@ -1,5 +0,0 @@
-from plugins import lmgtfy
-
-
-def test_lmgtfy(patch_try_shorten):
-    assert lmgtfy.lmgtfy("foo bar") == "http://lmgtfy.com/?q=foo%20bar"
