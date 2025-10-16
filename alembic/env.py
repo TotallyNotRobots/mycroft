37f3cf9c4e4c46d500769c8a28210a224c1ced10
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-10-16 21:43:04 +0000

    refactor(alembic): remove unused offline migration

--- alembic/env.py
+++ alembic/env.py
@@ -28,30 +28,6 @@ target_metadata = database.metadata
 # ... etc.
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
@@ -70,7 +46,4 @@ def run_migrations_online() -> None:
             context.run_migrations()
 
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+run_migrations_online()
